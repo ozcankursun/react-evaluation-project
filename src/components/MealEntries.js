349d31760d8662974fd8b8419ad0660fb155b538
ozcankursun <62161820+ozcankursun@users.noreply.github.com>
2021-05-20 00:07:53 +0300

    startup code

---
+++ src/components/MealEntries.js
@@ -0,0 +1,15 @@
+import "./MealEntries.css";
+const MealEntries = () => {
+  return (
+    <div className="meal-entries">
+      <div className="meal-entries-date">25/5/2020 14:40</div>
+      <div>
+        <div>Brief description of meal</div>
+        <div>Breakfast</div>
+      </div>
+      <div className="meal-entries-count">Calories of the dishes</div>
+     
+    </div>
+  );
+};
+export default MealEntries;
